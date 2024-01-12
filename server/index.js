5751d727e79bbb71db63e2e19c3c28ac27560f4b
Raiyan <ray38250968@gmal.com>
2024-01-12 16:09:41 +0530

    feat(register) : working but without test case

--- server/index.js
+++ server/index.js
@@ -1,25 +1,45 @@
 import express from "express";
-import path from "path";
-import { join } from "path";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+import User from "./model/User.js";
+import jwt from "jsonwebtoken";
 dotenv.config();
+console.log("process.env.MONGO_URL : ", process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("process.env.MONGO_URL : ", process.env.MONGO_URL);
+    console.log("connected to mongoDB");
+  });
 
 const app = express();
 
-app.use(express.static("public"));
-app.use("/public", express.static("public"));
-
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = join(__filename, "..");
-app.use(express.static(path.join(__dirname, "/index.html")));
-
+app.use(express.json());
 app.get("/api", (req, res) => {
   res.send("hello from the server");
+});
+
+app.post("/api/register", async (req, res) => {
+  const { username, password } = req.body;
+  console.log("username : ", username);
+  console.log("password : ", password);
+  const createdUser = await User.create({ username, password });
+  jwt.sign(
+    { userID: createdUser._id },
+    process.env.JWT_SECRET,
+    (err, token) => {
+      if (err) {
+        console.log("err : ", err);
+        throw err;
+      } else {
+        res.cookie("token", token);
+        res.status(201).json(user);
+      }
+    },
+  );
 });
 
 app.listen(process.env.PORT || 6968, () => {
